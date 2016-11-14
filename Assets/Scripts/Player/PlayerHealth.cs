88e805a049d0599c155deae52e74c2a344afdada
Nymua <duboiscorenthin@gmail.com>
2016-11-14 13:47:03 +0100

    Initiate

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int health = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public Image damageImage;
+	public float flashSpeed = 5f;
+	public Color flashColour = new Color(1f,0f,0f,0.1f);
+
+	Animator animator;
+	PlayerMovement playerMovement;
+	bool isDead;
+	bool damaged;
+}
