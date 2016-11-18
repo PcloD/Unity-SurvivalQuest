26ab6d2d54caa95ab27b5a3e982f02483b0a840c
DUBOIS Corenthin <cdubois15@STUD.IPL.DOM>
2016-11-18 15:55:13 +0100

    Ennemy Attack

--- Project/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Project/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class EnemyHealth : MonoBehaviour {
 
+    public float timeBetweenAttacks = 0.4f;
+    public int attackDamage = 10;
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    bool inRange;
+    float timer;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter (Collider collide) {
+	    if (collide.gameObject == player)
+        {
+
+        }
 	}
 }
