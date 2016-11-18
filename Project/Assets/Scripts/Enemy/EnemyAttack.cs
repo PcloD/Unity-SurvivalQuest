d9c532590224ed292223e790a4a61607f5b21e59
DUBOIS Corenthin <cdubois15@STUD.IPL.DOM>
2016-11-18 16:22:55 +0100

    Ennemy Attack Done

---
+++ Project/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 0.4f;
+    public int attackDamage = 10;
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    bool inRange;
+    float timer;
+
+	// Use this for initialization
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider collide) {
+	    if (collide.gameObject == player)
+        {
+            inRange = true;
+        }
+	}
+
+    void OnTriggerExit (Collider collide)
+    {
+        if (collide.gameObject == player)
+        {
+            inRange = false;
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeBetweenAttacks && inRange)
+        {
+            Attack();
+        }
+
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+    }
+
+    void Attack()
+    {
+        timer = 0f;
+        if (playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
