51e86fb2501c1e988c85d15ac93b15b95a4b5e37
Nymua <duboiscorenthin@gmail.com>
2016-11-18 11:22:16 +0100

    enemymovement script

--- Project/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Project/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class EnemyMovement : MonoBehaviour
+{
+	Transform player;              
+	NavMeshAgent nav;               
 
-	// Use this for initialization
-	void Start () {
-	
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <NavMeshAgent> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+	void Update ()
+	{
+			nav.SetDestination (player.position);
+	} 
 }
