179afd802856b51347d3470c0e6e620bfe1598c0
DUBOIS Corenthin <cdubois15@stud.ipl.dom>
2016-11-18 15:27:46 +0100

    PlayerHealth

--- Project/Assets/Scripts/Player/PlayerHealth.cs
+++ Project/Assets/Scripts/Player/PlayerHealth.cs
@@ -2,23 +2,56 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class PlayerHealth : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour
+{
 
-	public int health = 100;
-	public int currentHealth;
-	public Slider healthSlider;
-	public Image damageImage;
-	public float flashSpeed = 4f;
-	public Color flashColour = new Color(1f,0f,0f,0.1f);
+    public int health = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    //public Image damageImage;
+    //public float flashSpeed = 4f;
+    //public Color flashColour = new Color(1f,0f,0f,0.1f);
 
-	Animator animator;
-	PlayerMovement playerMovement;
-	bool isDead;
-	bool damaged;
+    Animator animator;
+    PlayerMovement playerMovement;
+    bool isDead;
+    bool damaged;
 
     void Awake()
     {
         animator = GetComponent<Animator>();
         playerMovement = GetComponentInChildren<PlayerMovement>();
+        currentHealth = health;
     }
+
+    void Update()
+    {
+        if (damaged)
+        {
+            //var fill = (healthSlider as UnityEngine.UI.Slider).GetComponentsInChildren<UnityEngine.UI.Image>();
+            //fill.color = Color.Lerp(Color.red, Color.green, 0.5);
+
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damaged = true;
+        currentHealth -= damage;
+        healthSlider.value = currentHealth;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        isDead = true;
+        animator.SetTrigger("Die");
+        playerMovement.enabled = false;
+    }
+
+
 }
